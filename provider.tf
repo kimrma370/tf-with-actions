432bb255f2a9af2abad94463cc550dc6fe6e2555
kimrma370 <kimrma370@gmail.com>
2025-06-25 15:29:27 +0900

    update cloud configuration

--- provider.tf
+++ provider.tf
@@ -5,12 +5,22 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  backend "s3" {
-    bucket         = "backend-bucket-kimrma0"
-    key            = "terraform/state-test/terraform.tfstate"
-    region         = "ap-northeast-2"
-    dynamodb_table = "terraform-lock"
-  }
+
+  cloud { 
+    
+    organization = "test-kimrma370" 
+
+    workspaces { 
+      name = "test-kimrma370" 
+    } 
+  } 
+
+  # backend "s3" {
+  #   bucket         = "backend-bucket-kimrma0"
+  #   key            = "terraform/state-test/terraform.tfstate"
+  #   region         = "ap-northeast-2"
+  #   dynamodb_table = "terraform-lock"
+  # }
 }
 
 provider "aws" {
